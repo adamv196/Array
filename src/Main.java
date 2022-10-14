dfdc30ff7175d6f0f8656de6f9d5f4b527590a9a
Adam Viglas <adamv196@gmail.com>
2022-10-14 18:37:22 +0200

    c

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] array1 = {5, 4, 2, 8, 3, 8, 4, 8, 9, 7, 2, 3, 4, 7,};
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + ", ");
+        }
+
+    }
+}
